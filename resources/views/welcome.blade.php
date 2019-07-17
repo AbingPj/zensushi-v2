8e8f8731bfbe4269f9cda994a4e04c3f6e7679e2
ZensushiDavao-Inv <zensushi.d@gmail.com>
2019-07-18 00:29:40 +0800

    Add Laravel Auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Zensushi Inventory
                 </div>
 
                 <div class="links">
