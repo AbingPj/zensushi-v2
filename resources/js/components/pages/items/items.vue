74e689ece6e0215966ef2c62b79c4ffdc8781eba
ZensushiDavao-Inv <zensushi.d@gmail.com>
2019-07-20 12:07:38 +0800

    Items: Add Vue Component

---
+++ resources/js/components/pages/items/items.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="row">
+        <h1>THIS IS ITEMS VUE COMPONENTS</h1>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
