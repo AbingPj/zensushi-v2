ec3d859a30b7e8c3676f19aac7519f54ef9ab6d4
ZensushiDavao-Inv <zensushi.d@gmail.com>
2019-07-20 15:27:10 +0800

    Add Model class

---
+++ app/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+
+    protected $guarded=[];
+
+    public function items()
+    {
+        return $this->hasMany('App\Item', 'category_id', 'id');
+    }
+}
