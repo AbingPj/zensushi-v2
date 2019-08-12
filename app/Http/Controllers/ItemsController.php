a28d548c898d4d177cdc5193ec3b768cfae64c7a
leopoldo <leopoldo.a@filipinowebmasters.com>
2019-08-12 14:44:50 +0800

    Update Create Item Page

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -2,93 +2,96 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 use App\Item;
 use App\Item_type;
+use App\Unit;
 
 class ItemsController extends Controller
 {
-    public function items(){
+    public function items()
+    {
         $items = Item::all();
         return response()->json($items);
     }
 
-    public function item_types(){
+    public function item_types()
+    {
         $items = Item_type::all();
-        return response()->json($items);   
+        return response()->json($items);
     }
 
-    public function createItem($id){
-       
-        $item = Item_type::find($id);
-        return view('app.pages.items.items-create', compact($item));
-       
+    public function createItem($id)
+    {
+
+        $itemType = Item_type::find($id);
+        $categories = Category::all();
+        $units = Unit::all();
+
+        return view('app.pages.items.items-create', compact('itemType', 'categories', 'units'));
     }
 
     public function index()
     {
         $request = request();
-        $query = Item::join('units','items.unit_id','units.id')
-                     ->join('users', 'items.user_id','users.id')
-                     ->join('categories','items.category_id','categories.id')
-                     ->join('item_types', 'items.item_type_id','item_types.id')
-                      ->select(
-                                'items.id AS id',
-                                'items.description AS description',
-                                'items.remove AS remove',
-                                'items.created_at AS created',
-                                'users.name AS created_by',
-                                'categories.description AS category',
-                                'item_types.description AS item_type',
-                                'units.description AS unit',
-                                'items.category_id',
-                                'items.item_type_id',
-                                'items.unit_id'
-                            )
-                     ->newQuery();
-            if (request('sort') != "" ){
-                // handle multisort
-                $sorts = explode(',', request()->sort);
-                foreach ($sorts as $sort) {
-                    list($sortCol, $sortDir) = explode('|', $sort);
-                    $query = $query->orderBy($sortCol, $sortDir);
-                }
-            } else {
-                $query = $query->orderBy('items.id', 'asc');
+        $query = Item::join('units', 'items.unit_id', 'units.id')
+            ->join('users', 'items.user_id', 'users.id')
+            ->join('categories', 'items.category_id', 'categories.id')
+            ->join('item_types', 'items.item_type_id', 'item_types.id')
+            ->select(
+                'items.id AS id',
+                'items.description AS description',
+                'items.remove AS remove',
+                'items.created_at AS created',
+                'users.name AS created_by',
+                'categories.description AS category',
+                'item_types.description AS item_type',
+                'units.description AS unit',
+                'items.category_id',
+                'items.item_type_id',
+                'items.unit_id'
+            )
+            ->newQuery();
+        if (request('sort') != "") {
+            // handle multisort
+            $sorts = explode(',', request()->sort);
+            foreach ($sorts as $sort) {
+                list($sortCol, $sortDir) = explode('|', $sort);
+                $query = $query->orderBy($sortCol, $sortDir);
             }
+        } else {
+            $query = $query->orderBy('items.id', 'asc');
+        }
 
 
-             if ($request->exists('filter')) {
-                 $query->where(function($q) use($request) {
-                     $value = "%{$request->filter}%";
-                     $q->where('items.id', 'like', $value)
-                         ->orWhere('items.description', 'like', $value)
-                         ->orWhere('categories.description', 'like', $value)
-                         ->orWhere('item_types.description', 'like', $value)
-                         ->orWhere('users.name', 'like', $value);
-                 });
-             }
+        if ($request->exists('filter')) {
+            $query->where(function ($q) use ($request) {
+                $value = "%{$request->filter}%";
+                $q->where('items.id', 'like', $value)
+                    ->orWhere('items.description', 'like', $value)
+                    ->orWhere('categories.description', 'like', $value)
+                    ->orWhere('item_types.description', 'like', $value)
+                    ->orWhere('users.name', 'like', $value);
+            });
+        }
 
 
-              $perPage = request()->has('per_page') ? (int) request()->per_page : null;
-              $pagination = $query->paginate($perPage);
-              $pagination->appends([
-                  'sort' => request()->sort,
-                  'filter' => request()->filter,
-                  'per_page' => request()->per_page
-              ]);
+        $perPage = request()->has('per_page') ? (int) request()->per_page : null;
+        $pagination = $query->paginate($perPage);
+        $pagination->appends([
+            'sort' => request()->sort,
+            'filter' => request()->filter,
+            'per_page' => request()->per_page
+        ]);
 
-            //   dd($pagination);
+        //   dd($pagination);
 
 
-            return response()->json(
-                   $pagination
-                )
-                ->header('Access-Control-Allow-Origin', '*')
-                ->header('Access-Control-Allow-Methods', 'GET');
+        return response()->json(
+            $pagination
+        )
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET');
     }
-
-
-
-
 }
--- resources/views/app/pages/items/items-create.blade.php
+++ resources/views/app/pages/items/items-create.blade.php
@@ -5,25 +5,11 @@
 @section('content')
   @include('layout.partials.banner')
    <div class="container">
-       <h1>
-       {{-- {{ $item }} --}}
-       </h1>
-       {{-- <h2>{{ $data.id }} --}}
-            
-       </h2>
-       <div class="row">
-           <div class="col">
-            <form action="">
-                <div class="form-group">
-                    <label for="">Item Type</label>
-                    <input type="text" name="item_type" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">Category</label>
-                    <select name="" id="category"></select>
-                </div>
-            </form>
-           </div>
-       </div>
+        
+    
+    <vc-items-create itemtype="{{ $itemType }}" categories="{{ $categories }}" units="{{ $units }}">
+        </vc-items-create>
+    
+    
    </div>
 @endsection
