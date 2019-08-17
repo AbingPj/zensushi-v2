cc3db1b4672ba5ec117299fd6ca11c95c4ef1c68
abingpj <pj.abing@gmail.com>
2019-08-18 00:11:52 +0800

    Add Loading Overlay Function

--- resources/views/layout/partials/footer-scripts.blade.php
+++ resources/views/layout/partials/footer-scripts.blade.php
@@ -1,3 +1,20 @@
 {{-- <!-- Scripts --> --}}
 <script src="{{ url('/js/app.js') }}"></script>
 {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+<script>
+function LoadingOverlay() {
+ var customElement = $("<img>", {
+     class: "ld ld-shadow",
+     src: "{{ asset('images/svg_logo/logo2.svg') }}"
+ });
+ $.LoadingOverlay("show", {
+     custom: customElement,
+     image: "",
+     imageAnimation: ""
+ });
+}
+function LoadingOverlayHide() {
+ $.LoadingOverlay("hide");
+}
+
+</script>
