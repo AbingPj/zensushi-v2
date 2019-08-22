4ec335a1c605edd79a64063a2fa76bddaaef7214
leopoldo <leopoldo.a@filipinowebmasters.com>
2019-08-22 10:24:58 +0800

    Add Content for Item Delete Modal Comp

---
+++ resources/js/components/pages/items/ItemsDelete.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="modal fade" id="deletePostModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <h4 class="modal-title">Delete Post</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+
+          <!-- Modal body -->
+          <div class="modal-body">
+            <p>
+              Are you sure you want to delete this item:
+              <br />Item:&nbsp;[ &nbsp;
+              <b>{{ item.description }}</b> &nbsp;]?
+            </p>
+            <center>
+              <button @click="deletePost()" class="btn btn-danger" data-dismiss="modal">Yes</button>
+
+              <button class="btn btn-secondary" data-dismiss="modal">No</button>
+            </center>
+          </div>
+
+          <!-- Modal footer -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object
+  },
+  methods: {
+    async deletePost() {
+      LoadingOverlay();
+      let { status } = await axios.delete("/posts/delete/" + this.post.id);
+      if (status == 200) {
+        this.$events.fire("refreshVueTable", status);
+        LoadingOverlayHide();
+      } else {
+        console.log("Failed Update");
+        LoadingOverlayHide();
+      }
+    }
+  }
+};
+</script>
