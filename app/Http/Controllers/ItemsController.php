736774ad18f5534fbe54ad2b32f22af6906fcf71
Leopoldo Abing <abing-leopoldo@hpi.local>
2019-10-24 17:44:55 +0800

    Update ItemsController

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -216,17 +216,25 @@ class ItemsController extends Controller
                 $in->date = $date;
                 $in->save();
                 broadcast(new ItemsEvent($item->id));
+            }elseif($item->item_type_id ==  3) {
+                $in = new In_record;
+                $in->item_id = $item->id;
+                $in->value = $value;
+                $in->user = Auth::id();
+                $in->date = $date;
+                $in->save();
+                broadcast(new ItemsEvent($item->id));
             }
         });
     }
 
     public function StockOutRaw(Request $request)
     {
-
         DB::transaction(function () use ($request) {
             $value = $request->input('value');
             $date = $request->input('date');
             $item = Item::findOrFail($request->input('itemId'));
+            
             if ($item->item_type_id ==  1) {
                 $out = new Out_record();
                 $out->item_id = $item->id;
@@ -235,7 +243,16 @@ class ItemsController extends Controller
                 $out->date = $date;
                 $out->save();
                 broadcast(new ItemsEvent($item->id));
+            }elseif($item->item_type_id == 3){
+                $out = new Out_record();
+                $out->item_id = $item->id;
+                $out->value = $value;
+                $out->user = Auth::id();
+                $out->date = $date;
+                $out->save();
+                broadcast(new ItemsEvent($item->id));
             }
+
         });
     }
 
