292a5c2c560b6e3e4c81000713007c008ef9aa66
Leopoldo Abing <abing-leopoldo@hpi.local>
2019-11-11 17:28:27 +0800

    Added New Controllers

---
+++ app/Http/Controllers/ItemsProductController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Raw_product;
+use App\Item;
+use App\Raw;
+use Illuminate\Http\Request;
+
+class ItemsProductController extends Controller
+{
+    
+    public function getProductItems(){
+        
+        $products = Raw_product::all();
+        if ($products->isNotEmpty()) {
+            $products->map(function ($row) {
+                $row->item = $row->item;
+                $row->unit = $row->item->unit;
+                $row->selected = false;
+                $row->quantity = 1;
+                $row->total_weight = $row->value;
+                return $row;
+            });
+        }
+        return response()->json($products);
+    }
+    
+    public function getProductItemsByRaw($items_id){
+        
+        $item = Item::findOrFail($items_id);
+        $raw = Raw::findOrFail($item->raw->id);
+        $raw_products = $raw->raw_products;
+        $raw_products->map(function ($row) {
+            return $row->item = $row->item;
+        });
+        $raw_products->map(function ($row) {
+            return $row->unit = $row->item->unit;
+        });
+        return response()->json(
+            $raw_products
+        );
+    }
+
+    
+    public function StockInRawProduct($item_id)
+    {
+        $item = Item::findOrFail($item_id);
+        $raw = Raw::findOrFail($item->raw->id);
+        $raw_products = $raw->raw_products;
+        $raw_products->map(function ($row) {
+            return $row->item = $row->item;
+        });
+        return response()->json(
+            $raw_products
+        );
+    }
+
+    public function StockInRawProduct2($item_id, $raw_product_id)
+    {
+        $item = Item::findOrFail($item_id);
+        $raw = Raw::findOrFail($item->raw->id);
+        $raw_products = $raw->raw_products;
+        $raw_products->map(function ($row) {
+            return $row->item = $row->item;
+        });
+        $selected_raw_product = Raw_product::findORFail($raw_product_id);
+        return response()->json(
+            $selected_raw_product
+        );
+        dd($selected_raw_product);
+    }
+}
