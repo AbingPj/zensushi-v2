675e1f1bb1f57db4b1eee808432668d65d57abfd
Leopoldo Abing <abing-leopoldo@hpi.local>
2019-11-20 14:23:34 +0800

    Update Login Controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/zensushi';
+    protected $redirectTo = '/zensushi-items';
 
     /**
      * Create a new controller instance.
