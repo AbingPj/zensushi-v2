34906a41e46397687d059690db9d7d52d1ed5982
Leopoldo Abing <abing-leopoldo@hpi.local>
2019-12-04 15:48:11 +0800

    Update Item Class

--- app/CustomizeClass/ItemClass.php
+++ app/CustomizeClass/ItemClass.php
@@ -30,38 +30,38 @@ class ItemClass
     return $balance;
   }
 
-  static function getItemBalanceWithSelectedDate($item_id, $date)
-  {
-    $balance = 0;
+  // static function getItemBalanceWithSelectedDate($item_id, $date)
+  // {
+  //   $balance = 0;
 
-    // $items_in = In_record::all()->where('item_id', $item_id);
+  //   // $items_in = In_record::all()->where('item_id', $item_id);
 
-    // $items_out = Out_record::all()->where('item_id', $item_id);
-    // $items_add = Additional::all()->where('item_id', $item_id);
+  //   // $items_out = Out_record::all()->where('item_id', $item_id);
+  //   // $items_add = Additional::all()->where('item_id', $item_id);
 
-    $items_in = DB::table('In_records')
-      ->where('item_id', $item_id)
-      ->whereDate('date', '<=', date($date))->get();
+  //   $items_in = DB::table('In_records')
+  //     ->where('item_id', $item_id)
+  //     ->whereDate('date', '<=', date($date))->get();
 
-    $items_out = DB::table('Out_records')
-      ->where('item_id', $item_id)
-      ->whereDate('date', '<=', date($date))->get();
+  //   $items_out = DB::table('Out_records')
+  //     ->where('item_id', $item_id)
+  //     ->whereDate('date', '<=', date($date))->get();
 
-    $items_add = DB::table('Additionals')
-      ->where('item_id', $item_id)
-      ->whereDate('date', '<=', date($date))->get();
+  //   $items_add = DB::table('Additionals')
+  //     ->where('item_id', $item_id)
+  //     ->whereDate('date', '<=', date($date))->get();
 
 
-    (!empty($items_in)) ?
-      $IN = $items_in->sum('value') : $IN = 0;
+  //   (!empty($items_in)) ?
+  //     $IN = $items_in->sum('value') : $IN = 0;
 
-    (!empty($items_out)) ?
-      $OUT = $items_out->sum('value') : $OUT = 0;
+  //   (!empty($items_out)) ?
+  //     $OUT = $items_out->sum('value') : $OUT = 0;
 
-    (!empty($items_add)) ?
-      $ADD = $items_add->sum('value') : $ADD = 0;
+  //   (!empty($items_add)) ?
+  //     $ADD = $items_add->sum('value') : $ADD = 0;
 
-    $balance = ($IN + $ADD) - $OUT;
-    return $balance;
-  }
+  //   $balance = ($IN + $ADD) - $OUT;
+  //   return $balance;
+  // }
 }
