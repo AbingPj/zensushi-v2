f99675934c0b19f020e279847592104e81055c1a
Leopoldo Abing <abing-leopoldo@hpi.local>
2019-12-09 15:40:25 +0800

    Update ItemsProduct Controller

--- app/Http/Controllers/ItemsProductController.php
+++ app/Http/Controllers/ItemsProductController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Bone;
 use App\In_record;
 use App\Raw_product;
 use App\Item;
+use App\Out_record;
 use App\Raw;
+use App\Scrap;
 use Auth;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -99,21 +103,63 @@ class ItemsProductController extends Controller
         DB::transaction(function () use ($request) {
             $bones = $request->input('bones');
             $scrap = $request->input('scrap');
+
             $total = $request->input('total');
+            $difference  = $request->input('difference');
+        
+
             $selected_raw = $request->input('selected_raw');
+            $selected_raw_item_id = $selected_raw['item_id'];
             $selected_raw_out_value = $request->input('selected_raw_out_value');
             $selected_products = $request->input('selected_products');
             $date = $request->input('date');
             // dd($selected_products);
-            foreach ($selected_products as $key => $product) {
+
             
+
+            $raw_out = new Out_record;
+            $raw_out->item_id = $selected_raw_item_id;
+            $raw_out->value = $selected_raw_out_value;
+            $raw_out->date = $date;
+            $raw_out->user  = Auth::id();
+            $raw_out->total_production = $total;
+            $raw_out->difference = $difference;
+            $raw_out->save();
+
+
+
+            $bones_obj = new Bone();
+            $bones_obj->item_id     = $selected_raw_item_id;
+            $bones_obj->value       = $bones;
+            $bones_obj->date        = $date;
+            $bones_obj->user        = Auth::id();
+            $bones_obj->raw_out_id  = $raw_out->id;
+            $bones_obj->save();
+            dump($bones);
+            dump($bones_obj);
+
+            $scrap_obj = new Scrap();
+            $scrap_obj->item_id     = $selected_raw_item_id;
+            $scrap_obj->value       = $scrap;
+            $scrap_obj->date        = $date;
+            $scrap_obj->user        = Auth::id();
+            $scrap_obj->raw_out_id  = $raw_out->id;
+            $scrap_obj->save();
+            dump($scrap);
+            dump($scrap_obj);
+
+
+            
+
+            foreach ($selected_products as $key => $product) {
                 $in          = new In_record;
                 $in->item_id = $product['item_id'];
                 $in->value   = $product['quantity'];
                 $in->user    = Auth::id();
                 $in->date    = $date;
+                $in->raw_out_id = $raw_out->id;
                 $in->save();
-             }
+            }
             // $value = $request->input('value');
             // $date = $request->input('date');
             // $item = Item::findOrFail($request->input('itemId'));
