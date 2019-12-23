d13a4862454a0adfd528153082f697bbdcf4c47a
Leopoldo Abing <abing-leopoldo@hpi.local>
2019-12-23 13:40:19 +0800

    Update Records Event

--- app/Events/RecordsEvent.php
+++ app/Events/RecordsEvent.php
@@ -10,17 +10,19 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class RecordsEvent
+
+class RecordsEvent implements ShouldBroadcast
 {
+
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $data;
 
     public function __construct($data)
     {
-       
+
         $this->data = $data;
-       
+        
     }
 
     public function broadCastWith()
