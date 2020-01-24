2173b3c5a303ac46b60f30aad601c931efd756b8
Leopoldo Abing <abing-leopoldo@hpi.local>
2020-01-24 13:30:00 +0800

    Notification Vue Component PAge

--- resources/views/app/pages/notification/notification.blade.php
+++ resources/views/app/pages/notification/notification.blade.php
@@ -1,7 +1,8 @@
 
-<?php $bannerheader = "Notification"; ?>
+<?php $bannerheader = "Notifications"; ?>
 @extends('layout.app')
 
 @section('content')
   @include('layout.partials.banner')
+    <notifications-page></notifications-page>
 @endsection
