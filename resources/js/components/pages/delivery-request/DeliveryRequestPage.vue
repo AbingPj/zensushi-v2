c97a07696a948416f64631db5915e224a083ae81
Leopoldo Abing <abing-leopoldo@hpi.local>
2020-01-30 17:10:25 +0800

    Update front end page

--- resources/js/components/pages/delivery-request/DeliveryRequestPage.vue
+++ resources/js/components/pages/delivery-request/DeliveryRequestPage.vue
@@ -1,9 +1,9 @@
 <template>
    <div class="container">
       <div class="row">
-         <div class="col-md-5">
+         <div class="col-md-4">
             <h3>Select Products</h3>
-            <small>Select product to make request delivery list</small>
+            <small>Select product for request delivery list</small>
             <div class="input-group mb-3">
                <input
                   v-model="search_product"
@@ -15,7 +15,7 @@
                   <button class="btn btn-primary" type="button" @click="search()">Search</button>
                </div>
             </div>
-            <table class="table table-striped">
+            <table class="table">
                <thead>
                   <th>Item id</th>
                   <th>Item Name</th>
@@ -28,15 +28,54 @@
                      <td>{{product.description}}</td>
                      <td>{{product.balance}} {{product.unit_desc}}</td>
                      <td>
-                        <button class="btn btn-success btn-sm">Select</button>
+                        <button
+                           class="btn btn-success btn-sm"
+                           @click="selectProduct(product)"
+                        >Select</button>
                      </td>
                   </tr>
                </tbody>
             </table>
          </div>
 
-         <div class="col-md-7">
-            <h3 class="text-center">Delivery Reqquest Form</h3>
+         <div class="col-md-8">
+            <h3 class="text-center">Delivery Reqquest List Form</h3>
+            <table class="table table-striped">
+               <thead>
+                  <th>Item id</th>
+                  <th>Item Name</th>
+                  <th style="width: 150px;">Quantity</th>
+                  <th></th>
+               </thead>
+               <tbody>
+                  <tr v-for="product in selectedProducts" :key="product.id">
+                     <td>{{product.id}}</td>
+                     <td>{{product.description}}</td>
+                     <td>
+                        <div class="input-group input-group-sm">
+                           <input
+                              type="number"
+                              style="text-align:center;"
+                              class="form-control"
+                              v-model="product.quantity"
+                              min="1"
+                              max="100"
+                              onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))"
+                           />
+                           <!-- :value="product.quantity" -->
+                           <div class="input-group-append">
+                              <span class="input-group-text">{{product.unit_desc}}</span>
+                           </div>
+                        </div>
+                     </td>
+                     <td>
+                        <button class="btn btn-danger" @click="deleteProduct(product)">
+                           <i class="fa fa-eraser" aria-hidden="true"></i>
+                        </button>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
          </div>
       </div>
    </div>
@@ -48,10 +87,30 @@ export default {
    data() {
       return {
          search_product: "",
-         products: []
+         products: [],
+         selectedProducts: []
       };
    },
+
    methods: {
+      selectProduct(data) {
+         let y = this.selectedProducts.find(list => list.id == data.id);
+         if (y == undefined) {
+            data.quantity = 1;
+            this.selectedProducts.push(data);
+         }
+      },
+      deleteProduct(data) {
+         // let newList = this.selectedProducts.filter(
+         //    list => list.id != data.id
+         // );
+
+         let newList = this.selectedProducts.filter(function(list) {
+            return list.id != data.id;
+         });
+         this.selectedProducts = newList;
+      },
+
       async getProducts() {
          let { data, status } = await axios.get("/items/delivery/products");
          if (data && status == 200) {
@@ -60,8 +119,10 @@ export default {
             alert("Something went wrong! Please try again.");
          }
       },
-      async search(){
-         let { data, status } = await axios.get("/items/delivery/products/"+this.search_product);
+      async search() {
+         let { data, status } = await axios.get(
+            "/items/delivery/products/" + this.search_product
+         );
          if (data && status == 200) {
             this.products = data;
          } else {
