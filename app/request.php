7516d663585515ef0cd84ba1c88d3995d460be93
Leopoldo Abing <abing-leopoldo@hpi.local>
2020-02-17 18:04:57 +0800

    Update Models

--- app/request.php
+++ app/request.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class request extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function request_lists()
+    {
+        return $this->hasMany(request_list::class);
+    }
 }
--- app/request_list.php
+++ app/request_list.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class request_list extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function request()
+    {
+        return $this->belongsTo(request::class);
+    }
 }
+
