e197ae4e507ec73c7b84acf42b37182b239feb86
AbingPj <pj.abing@gmail.com>
2020-04-19 20:58:57 +0800

    Added New Design Template AdminLte

---
+++ resources/views/layout2/parts/head.blade.php
@@ -0,0 +1,13 @@
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="stylesheet" href="{{ asset('css/admin.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+    <link rel="icon" href="{{ asset('images/zenicon.png') }}">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
