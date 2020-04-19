72a7392ea23237e203ed1ff54da45d1abe6284cd
AbingPj <pj.abing@gmail.com>
2020-04-19 19:51:48 +0800

    Update Web Pack

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/admin.scss', 'public/css/admin.css')
     .copyDirectory(
         "resources/js/bootstrap-notify-master/bootstrap-notify.min.js",
         "public/js"
@@ -20,5 +21,8 @@ mix.js('resources/js/app.js', 'public/js')
     .copyDirectory(
         "resources/sass/css/animate.css",
         "public/css"
+    ).copyDirectory(
+        "node_modules/admin-lte/dist/img",
+        "public/images/adminlte"
     );
-    
+
