1cf717f7b7d92b3fdd3f056f09add9f50ef230f1
AbingPj <pj.abing@gmail.com>
2020-04-19 22:27:37 +0800

    Updated Page Controller and web routes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -47,7 +47,7 @@ class PageController extends Controller
         return view(
                     'app.pages.items.items-raw-product-in',
                     compact('item_id', 'item_raw_stock_out', 'product_item_id')
-                    );
+        );
     }
     public function production2($item_id)
     {
@@ -80,6 +80,68 @@ class PageController extends Controller
 
     public function zen()
     {
-        return view('layout2.master');
+        return view('pages.home.home');
+    }
+    public function zenInventory()
+    {
+        return view('pages.items.items');
+    }
+    public function zenRecords()
+    {
+        return view('pages.records.records');
+    }
+
+    public function zenRequest()
+    {
+        return view('pages.delivery-request.delivery-request');
+    }
+
+    public function zenDelivery()
+    {
+        return view('pages.delivery.delivery');
+    }
+    public function zenUsers()
+    {
+        return view('pages.users.users');
+    }
+
+
+    public function zenProduction()
+    {
+        $item_id = null;
+        $item_raw_stock_out = null;
+        $product_item_id = null;
+        return view(
+                    'pages.items.items-raw-product-in',
+                    compact('item_id', 'item_raw_stock_out', 'product_item_id')
+        );
+    }
+    public function zenProduction2($item_id)
+    {
+
+        $item_raw_stock_out = null;
+        $product_item_id = null;
+        return view(
+                    'pages.items.items-raw-product-in',
+                    compact('item_id', 'item_raw_stock_out', 'product_item_id')
+                    );
+    }
+    public function zenProduction3( $item_id, $product_item_id )
+    {
+
+        $item_raw_stock_out = null;
+        return view(
+                    'pages.items.items-raw-product-in',
+                    compact('item_id', 'item_raw_stock_out', 'product_item_id')
+                    );
+    }
+
+    public function zenProduction4( $item_id, $product_item_id, $item_raw_stock_out )
+    {
+
+        return view(
+                    'pages.items.items-raw-product-in',
+                    compact('item_id', 'item_raw_stock_out', 'product_item_id')
+                    );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
+use Illuminate\Routing\RouteGroup;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,7 +38,20 @@ Route::get('/zensushi-production/{item_id}', 'PageController@production2');
 Route::get('/zensushi-production/{item_id}/{product_item_id}', 'PageController@production3');
 Route::get('/zensushi-production/{item_id}/{product_item_id}/{item_raw_stock_out}', 'PageController@production4');
 
-Route::get('/zen', 'PageController@zen')->name('zen.home');
+
+Route::group(['prefix' => 'zen'], function () {
+    Route::get('/', 'PageController@zen')->name('zen.home');
+    Route::get('/iventory', 'PageController@zenInventory')->name('zen.inventory');
+    Route::get('/records', 'PageController@zenRecords')->name('zen.records');
+    Route::get('/request', 'PageController@zenRequest')->name('zen.request');
+    Route::get('/delivery', 'PageController@zenDelivery')->name('zen.delivery');
+    Route::get('/users', 'PageController@zenUsers')->name('zen.users');
+    Route::get('/production', 'PageController@zenProduction')->name('zen.production');
+    Route::get('/production/{item_id}', 'PageController@zenProduction2');
+    Route::get('/production/{item_id}/{product_item_id}', 'PageController@zenProduction3');
+    Route::get('/production/{item_id}/{product_item_id}/{item_raw_stock_out}', 'PageController@zenProduction4');
+});
+
 
 
 // Items Controller
@@ -94,5 +108,3 @@ Route::get('loading', function () {
 
 Route::get('/test', 'TestController@test');
 Route::get('/test2', 'TestController@test2');
-
-
