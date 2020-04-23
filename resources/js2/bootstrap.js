279543f2c40b42667f6a4cc461bf04f331162422
AbingPj <pj.abing@gmail.com>
2020-04-23 23:18:09 +0800

    Add jquery datatables bootstrap 4

--- resources/js2/bootstrap.js
+++ resources/js2/bootstrap.js
@@ -4,11 +4,13 @@ try {
     window.$ = window.jQuery = require("jquery");
     require("bootstrap");
     require('admin-lte');
+    require('admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.js');
 } catch (e) { }
+
+
 window.axios = require("axios");
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 let token = document.head.querySelector('meta[name="csrf-token"]');
-
 if (token) {
     window.axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
 } else {
