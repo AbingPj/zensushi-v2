67f97c82857bc88f0797959b73d7f81932982066
AbingPj <pj.abing@gmail.com>
2020-04-26 00:36:04 +0800

    Add Icons At Title Page

--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                <h1 class="m-0 text-dark">{{$page_title}}</h1>
+                <h1 class="m-0 text-dark"><i class="fas fa-tachometer-alt"></i> {{$page_title}}</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
