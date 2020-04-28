1f21221e4cc5972dc4fd7f0093b40b3141138c2d
AbingPj <pj.abing@gmail.com>
2020-04-28 23:46:15 +0800

    Added table display and function v compont records

--- resources/js2/pages/Records/index.vue
+++ resources/js2/pages/Records/index.vue
@@ -7,6 +7,45 @@
                     <div class="col">
                         <div class="card card-primary card-outline">
                             <div class="card-body">
+                                <div class="table-responsive">
+                                    <table
+                                        id="dt"
+                                        class="table table-bordered table-sm table-hover"
+                                    >
+                                        <thead>
+                                            <tr>
+                                                <th style="width: 100px;" >Date</th>
+                                                <th>Item ID</th>
+                                                <th>Item</th>
+                                                <th>Add</th>
+                                                <th>IN</th>
+                                                <th>OUT</th>
+                                                <th>BONES</th>
+                                                <th>SCRAPS</th>
+                                                <th>TOTAL PROD</th>
+                                                <th>DIFFERENCE</th>
+                                                <th style="width: 100px;">Option</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr v-for="(item, index) in items" :key="index">
+                                                <td>{{item.date}}</td>
+                                                <td>{{item.item_id}}</td>
+                                                <td>{{item.item}}</td>
+                                                <td>{{item.ADD}}</td>
+                                                <td>{{item.IN}}</td>
+                                                <td>{{item.OUT}}</td>
+                                                <td>{{item.BONES}}</td>
+                                                <td>{{item.SCRAPS}}</td>
+                                                <td>{{item.TOTAL}}</td>
+                                                <td>{{item.DIFFERENCE}}</td>
+                                                <td>
+                                                    <!-- <records-action-button :item="item" :index="index"></action-button> -->
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -16,3 +55,50 @@
         <!-- /.content -->
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: []
+        };
+    },
+    created() {
+        this.getRecords();
+    },
+    methods: {
+        getRecords() {
+            axios
+                .get("/api/getRecords")
+                .then(res => {
+                    this.items = res.data;
+                    this.datatablesInitialize();
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        },
+        datatablesInitialize() {
+            $(document).ready(function() {
+                $("#dt").DataTable({
+                    language: { emptyTable: "nothing to see here" },
+                    order: [[1, "asc"]],
+                    lengthMenu: [
+                        [5, 10, 25, 50, -1],
+                        [5, 10, 25, 50, "All"]
+                    ],
+                    iDisplayLength: -1
+                });
+            });
+        },
+        editAction(data, index) {
+            let dataToUpdate = { ...data };
+            this.$events.fire("updateRecordModal", dataToUpdate);
+        },
+        deleteAction(data, index) {
+            let dataToDelete = { ...data };
+            this.$events.fire("deleteRecordModal", dataToDelete);
+        }
+    }
+};
+</script>
