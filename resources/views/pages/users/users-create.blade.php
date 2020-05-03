843a002fff1f03cd219d297a73ac621579372f72
AbingPj <pj.abing@gmail.com>
2020-05-03 22:30:16 +0800

    added create user page

---
+++ resources/views/pages/users/users-create.blade.php
@@ -0,0 +1,34 @@
+
+@extends('layout2.master')
+
+@section('title', 'Create User' )
+
+@section('content')
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                    <h1 class="m-0 text-dark"><i class="fas fa-th"></i> {{$page_title}}</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item">
+                                <a href="{{route('zen.home')}}">Home</a>
+                            </li>
+                            <li class="breadcrumb-item">
+                                <a href="{{route('zen.inventory')}}">{{$page_title}}</a>
+                            </li>
+                            <li class="breadcrumb-item active">Create New User</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- <items-create
+            itemtype="{{ $itemType }}"
+            categories="{{ $categories }}"
+            units="{{ $units }}"
+            rawitems="{{ $rawItems }}">
+        </items-create> --}}
+
+@endsection
