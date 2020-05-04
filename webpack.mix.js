57004306b1ac398b9a9a8bd5d94685f0c86d934f
AbingPj <pj.abing@gmail.com>
2020-05-04 22:16:40 +0800

    Update Webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js2/app.js', 'public/js/admin.js')
+mix.js('resources/js/app.js', 'public/js/admin.js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/admin.scss', 'public/css/admin.css')
     .copyDirectory(
